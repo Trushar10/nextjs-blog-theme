68fe2076bbca8f53c34350b9380fe004178e1fab
Trushar10 <trushargavit@gmail.com>
2025-04-22 10:48:07 +0530

    minor changes

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <img
-        src="public/images/profile-circle.png"
+        src="/images/profile-circle.png"
         alt="Logo"
-        className="block w-[50px] h-[50px] mx-auto mb-4 rounded-full"
+        className="block w-[100px] h-[100px] mx-auto mb-4 rounded-full"
       />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
